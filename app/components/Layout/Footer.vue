38f44839c5a1f516cab6981d53230b70dd8a29b6
clemcornet <cornet.clement@gmail.com>
2025-10-26 16:20:49 +0100

    add dynamic icons on footer

--- app/components/Layout/Footer.vue
+++ app/components/Layout/Footer.vue
@@ -1,21 +1,48 @@
 <script setup lang="ts">
 const { data } = useLazyFetch('/api/commits/last')
+
+const lastUpdated = computed(() => data.value?.date ? formatDate(data.value?.date, 'MMM d, yyyy') : '')
+
 const currentTime = computed(() => getCurrentTimeInTimeZone('Europe/Paris'))
-const lastUpdated = computed(() => data.value?.date ? formatDate(data.value?.date, 'MMMM d, yyyy') : '')
+const dayPeriod = computed(() => getDayPeriod(getDateInTimeZone('Europe/Paris')))
+const dayPeriodIcon = computed(() => {
+  switch (dayPeriod.value) {
+    case 'morning':
+      return 'i-iconoir-sea-and-sun'
+    case 'afternoon':
+      return 'i-iconoir-sun-light'
+    case 'night':
+      return 'i-iconoir-moon-sat'
+    default:
+      return ''
+  }
+})
 </script>
 
 <template>
-  <footer class="flex flex-col gap-4">
+  <footer class="flex flex-col gap-4 text-sm">
     <USeparator
       class="h-px"
       color="neutral"
     />
 
-    <div class="font-light text-sm flex justify-between">
-      <p>Last updated on {{ lastUpdated || '...' }}</p>
+    <div class="font-light flex justify-between">
+      <div class="flex gap-1 items-center">
+        <UIcon
+          class="size-4 text-neutral-500"
+          name="i-iconoir-git-merge"
+        />
+        <p>Last updated on {{ lastUpdated || '...' }}</p>
+      </div>
 
       <ClientOnly>
-        <p>Paris, {{ currentTime }}</p>
+        <div class="flex gap-1 items-center">
+          <UIcon
+            class="size-4 text-neutral-500"
+            :name="dayPeriodIcon"
+          />
+          <p>Paris, {{ currentTime }}</p>
+        </div>
       </ClientOnly>
     </div>
   </footer>
--- app/utils/date.ts
+++ app/utils/date.ts
@@ -1,4 +1,4 @@
-import { format as formatFns } from 'date-fns'
+import { format as formatFns, getHours } from 'date-fns'
 import { TZDate } from '@date-fns/tz'
 
 export const formatDate = (date: Date | string, format = 'do MMM, yyy') => {
@@ -11,4 +11,22 @@ export const formatDate = (date: Date | string, format = 'do MMM, yyy') => {
 export const getCurrentTimeInTimeZone = (timeZone: string, format = 'h:mm a') => {
   const tzDate = new TZDate(new Date(), timeZone)
   return formatFns(tzDate, format)
+}
+
+export const getDateInTimeZone = (timeZone: string) => {
+  return new TZDate(new Date(), timeZone)
+}
+
+export const getDayPeriod = (date: Date) => {
+  const hours = getHours(date)
+
+  if (hours >= 6 && hours < 12) {
+    return 'morning'
+  }
+  else if (hours >= 12 && hours < 18) {
+    return 'afternoon'
+  }
+  else {
+    return 'night'
+  }
 }
